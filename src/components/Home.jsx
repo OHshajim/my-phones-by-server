853534c459db072e66f7589339f206e41b32a785
OHshajim <ajshajimmax@gmail.com>
2024-04-21 07:28:11 +0600

    files setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Phones from "./Phones";
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <Phones/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
